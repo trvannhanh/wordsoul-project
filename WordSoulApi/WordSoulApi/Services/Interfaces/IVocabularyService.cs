773a0f05c44745d1440b96f20800087a8cb209d9
trvannhanh <trvannhanh@gmail.com>
2025-08-16 23:47:48 +0700

    first CRUD & search vocabularies

---
+++ WordSoulApi/WordSoulApi/Services/Interfaces/IVocabularyService.cs
@@ -0,0 +1,14 @@
+﻿using WordSoulApi.Models.DTOs.Vocabulary;
+
+namespace WordSoulApi.Services.Interfaces
+{
+    public interface IVocabularyService
+    {
+        Task<AdminVocabularyDto> CreateVocabularyAsync(AdminVocabularyDto vocabularyDto);
+        Task<bool> DeleteVocabularyAsync(int id);
+        Task<IEnumerable<AdminVocabularyDto>> GetAllVocabulariesAsync();
+        Task<IEnumerable<AdminVocabularyDto>> GetVocabulariesByWordsAsync(SearchVocabularyDto searchVocabularyDto);
+        Task<AdminVocabularyDto?> GetVocabularyByIdAsync(int id);
+        Task<AdminVocabularyDto> UpdateVocabularyAsync(int id, AdminVocabularyDto vocabularyDto);
+    }
+}
