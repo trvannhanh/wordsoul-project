5ee8dd8ba42e115b59047ec51a0f6d5163f59509
Tran Van Nhanh <140804671+trvannhanh@users.noreply.github.com>
2025-09-24 00:34:51 +0700

    Update Program.cs

--- WordSoulApi/WordSoulApi/Program.cs
+++ WordSoulApi/WordSoulApi/Program.cs
@@ -44,7 +44,7 @@ if (builder.Environment.IsDevelopment())
 }
 else
 {
-    var pgConn = Environment.GetEnvironmentVariable("DefaultConnection");
+    var pgConn = Environment.GetEnvironmentVariable("DATABASE_URL");
     builder.Services.AddDbContext<WordSoulDbContext>(options =>
         options.UseNpgsql(pgConn));
 }
