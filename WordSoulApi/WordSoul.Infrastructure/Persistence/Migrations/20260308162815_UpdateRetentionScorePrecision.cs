e6bb5a7adbf189ac72545e00abe632fcae31a45a
trvannhanh <trvannhanh@gmail.com>
2026-03-09 00:41:35 +0700

    fix: fix RetentionScore out of range & Grade Calculate

---
+++ WordSoulApi/WordSoul.Infrastructure/Persistence/Migrations/20260308162815_UpdateRetentionScorePrecision.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WordSoul.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRetentionScorePrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RetentionScore",
+                table: "UserVocabularyProgresses",
+                type: "decimal(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,4)",
+                oldPrecision: 5,
+                oldScale: 4);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RetentionScore",
+                table: "UserVocabularyProgresses",
+                type: "decimal(5,4)",
+                precision: 5,
+                scale: 4,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)",
+                oldPrecision: 5,
+                oldScale: 2);
+        }
+    }
+}
